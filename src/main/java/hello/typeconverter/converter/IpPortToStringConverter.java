d3aacfda24125f08204b4c70fa812caff1757418
Leenclair <leesu9317@gmail.com>
2023-02-20 23:05:00 +0900

    23/02/20 typeconverter End

--- src/main/java/hello/typeconverter/converter/IpPortToStringConverter.java
+++ src/main/java/hello/typeconverter/converter/IpPortToStringConverter.java
@@ -6,11 +6,10 @@ import org.springframework.core.convert.converter.Converter;
 
 @Slf4j
 public class IpPortToStringConverter implements Converter<IpPort, String> {
-
     @Override
     public String convert(IpPort source) {
-        //IpPort 객체 -> "127.0.0.1:8080"
         log.info("convert source={}", source);
+        //IpPort 객체 -> "127.0.0.1:8080"
         return source.getIp() + ":" + source.getPort();
     }
 }
